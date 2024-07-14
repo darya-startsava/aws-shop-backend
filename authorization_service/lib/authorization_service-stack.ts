385253aab527d53564dfa1c63dcd9686aa2f4b92
darya-startsava <daryastartsava@gmail.com>
2024-07-14 19:03:16 +0200

    feat: add basicAuthorizer function, update import

--- authorization_service/lib/authorization_service-stack.ts
+++ authorization_service/lib/authorization_service-stack.ts
@@ -1,16 +1,26 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import { Construct } from "constructs";
 
 export class AuthorizationServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AuthorizationServiceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const basicAuthorizerFunction = new lambda.Function(this, 'basicAuthorizer', {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler: 'basicAuthorizer.handler',
+      code: lambda.Code.fromAsset("lambda"),
+    });
+
+    basicAuthorizerFunction.grantInvoke(new iam.ServicePrincipal('apigateway.amazonaws.com'));
+
+
+    new cdk.CfnOutput(this, 'BasicAuthorizerFunctionArnOutput', {
+      value: basicAuthorizerFunction.functionArn,
+      description: 'ARN of the Basic Authorizer Lambda function',
+      exportName: 'BasicAuthorizerFunctionArn',
+    });
   }
 }
